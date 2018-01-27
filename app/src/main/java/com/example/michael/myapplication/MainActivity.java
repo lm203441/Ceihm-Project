2f61c7197e16f1bdb85ee99a2a04b7021abc4544
lm203441 <lm203441@etu.unice.fr>
2018-01-27 19:59:24 +0100

    proto implementation

--- app/src/main/java/com/example/michael/myapplication/MainActivity.java
+++ app/src/main/java/com/example/michael/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.michael.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +27,36 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+        final ImageButton traffic = (ImageButton) findViewById(R.id.trafficjam);
+        final ImageButton accident = (ImageButton) findViewById(R.id.accident);
+        final ImageButton closedroad = (ImageButton) findViewById(R.id.closedroad);
+
+        traffic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                traffic.setImageResource(R.drawable.ic_traffic_red);
+                accident.setImageResource(R.drawable.ic_car_collision);
+                closedroad.setImageResource(R.drawable.ic_no_stopping);
+            }
+        });
+
+        accident.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                traffic.setImageResource(R.drawable.ic_traffic);
+                accident.setImageResource(R.drawable.ic_car_collision_red);
+                closedroad.setImageResource(R.drawable.ic_no_stopping);
+            }
+        });
+
+        closedroad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                traffic.setImageResource(R.drawable.ic_traffic);
+                accident.setImageResource(R.drawable.ic_car_collision);
+                closedroad.setImageResource(R.drawable.ic_no_stopping_red);
             }
         });
     }
