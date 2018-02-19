55d7fc2f89e1d6002f0d2eb310183713d93ebe9f
Michael Liechtensteger <lm203441@etu.unice.fr>
2018-02-19 21:34:53 +0100

    you can now click on text (same effect as the buttons)

--- app/src/main/java/com/example/michael/myapplication/MainActivity.java
+++ app/src/main/java/com/example/michael/myapplication/MainActivity.java
@@ -6,11 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private boolean incident = false;
+    private boolean localisation = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,20 +29,67 @@ public class MainActivity extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
+        final TextView textLocalisation = (TextView) findViewById(R.id.textLocalisation);
+        final TextView textAccident = (TextView) findViewById(R.id.textAccident);
+        final TextView textEmbouteillage = (TextView) findViewById(R.id.textEmbouteillage);
+        final TextView textClosedroad = (TextView) findViewById(R.id.textClosedroad);
         final ImageButton traffic = (ImageButton) findViewById(R.id.trafficjam);
         final ImageButton accident = (ImageButton) findViewById(R.id.accident);
         final ImageButton closedroad = (ImageButton) findViewById(R.id.closedroad);
         final ImageButton globe = (ImageButton) findViewById(R.id.globe);
         final Button envoyer = (Button) findViewById(R.id.envoyer);
 
+        textAccident.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        incident = true;
+                        traffic.setImageResource(R.drawable.ic_traffic);
+                        accident.setImageResource(R.drawable.ic_car_collision_red);
+                        closedroad.setImageResource(R.drawable.ic_no_stopping);
+                    }
+                }
+        );
+
+        textEmbouteillage.setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    incident = true;
+                    traffic.setImageResource(R.drawable.ic_traffic_red);
+                    accident.setImageResource(R.drawable.ic_car_collision);
+                    closedroad.setImageResource(R.drawable.ic_no_stopping);
+                }
+            }
+        );
+
+        textClosedroad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                incident = true;
+                traffic.setImageResource(R.drawable.ic_traffic);
+                accident.setImageResource(R.drawable.ic_car_collision);
+                closedroad.setImageResource(R.drawable.ic_no_stopping_red);
+            }
+        });
+
+        textLocalisation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                localisation = true;
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+                globe.setImageResource(R.drawable.ic_earth_globe_with_continents_maps_red);
+            }
+        });
+
         envoyer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(incident){
+                if(incident && localisation){
                     startActivity(new Intent(MainActivity.this, ValidationActivity.class));
                 }
                 else{
-                    Toast.makeText(getApplicationContext(),"Veuillez choisir un type d'incident",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(),"Veuillez choisir un type d'incident et une localisation",Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -78,7 +127,9 @@ public class MainActivity extends AppCompatActivity {
         globe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                localisation = true;
                 startActivity(new Intent(MainActivity.this, MapsActivity.class));
+                globe.setImageResource(R.drawable.ic_earth_globe_with_continents_maps_red);
             }
         });
     }
